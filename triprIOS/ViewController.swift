cc284e479ccaf5b0790e16df621012d5a8ff566f
Daniel Skevarp <daniel.skevarp@fabularis.se>
2018-01-02 09:59:37 +0100

    Initial Commit

---
+++ triprIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  triprIOS
+//
+//  Created by Daniel Skevarp on 2018-01-02.
+//  Copyright © 2018 Daniel Skevarp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
