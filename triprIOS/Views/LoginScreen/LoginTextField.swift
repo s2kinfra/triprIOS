8f4bfef21904503852d3be6cda37c87e0841c3e5
Daniel Skevarp <daniel.skevarp@fabularis.se>
2018-04-05 06:29:57 +0200

    redesign
    redesign

---
+++ triprIOS/Views/LoginScreen/LoginTextField.swift
@@ -0,0 +1,36 @@
+//
+//  LoginTextField.swift
+//  triprIOS
+//
+//  Created by Daniel Skevarp on 2018-04-03.
+//  Copyright © 2018 Daniel Skevarp. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class LoginTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.borderColor = UIColor(white: 231/255, alpha: 1).cgColor
+        self.layer.borderWidth = 1
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 0, dy: 0)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds:bounds)
+    }
+
+}
